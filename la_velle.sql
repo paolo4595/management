36cce2d8acde132c1650a9aee2a5556690314371
paolo4595 <56024803+paolo4595@users.noreply.github.com>
2019-10-01 21:19:57 +0800

    Add files via upload

---
+++ la_velle.sql
@@ -0,0 +1,250 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 24, 2019 at 01:21 PM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+08:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES latin1 */;
+
+--
+-- Database: `la_velle`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
+(1, 'admin@gmail.com', '12345');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `create_account`
+--
+
+CREATE TABLE `create_account` (
+  `id` int(11) NOT NULL,
+  `name` char(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `mobile` bigint(20) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `gender` enum('male','female','other') NOT NULL,
+  `country` varchar(50) NOT NULL,
+  `picture` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `create_account`
+--
+
+INSERT INTO `create_account` (`id`, `name`, `email`, `password`, `mobile`, `address`, `gender`, `country`, `picture`) VALUES
+(1, 'Stephanie C. Luz', 'stephanieluz06@gmail.com', 'supersteph1234', 9452854083, 'Blk. 1 Lot 4 Avisea subdivision', 'female', 'Philippines', 'logo.jpg'),
+(13, 'Paolo', 'paolo4595@rocketmail.com', 'paoloqweqwe45', 9202736092, 'blk 1 lot 4 avisea', 'male', 'Philippines', 'drip.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `details_slider`
+--
+
+CREATE TABLE `details_slider` (
+  `id` int(11) NOT NULL,
+  `img` varchar(255) NOT NULL,
+  `caption` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `mobile` bigint(20) NOT NULL,
+  `message` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`id`, `name`, `email`, `mobile`, `message`) VALUES
+(1, 'Stephanie', 'stephanieluz06@gmail.com', 9452854083, 'Nice');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rooms`
+--
+
+CREATE TABLE `rooms` (
+  `room_id` int(11) NOT NULL,
+  `room_no` int(11) NOT NULL,
+  `type` varchar(100) NOT NULL,
+  `price` bigint(20) NOT NULL,
+  `details` text NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `service_reservation_details`
+--
+
+CREATE TABLE `service_reservation_details` (
+  `id` int(11) NOT NULL,
+  `name` char(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone` bigint(20) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `country` varchar(50) NOT NULL,
+  `service_type` varchar(100) NOT NULL,
+  `reservation_date` date NOT NULL,
+  `reservation_time` varchar(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data for table `service_reservation_details`
+--
+
+INSERT INTO `service_reservation_details` (`id`, `name`, `email`, `phone`, `address`, `country`, `service_type`, `reservation_date`, `reservation_time`) VALUES
+('1', 'Stephanie C. Luz', 'stephanieluz06@gmail.com', 9452854083, 'Blk. 1 Lot 4 Avisea subdivision, Bignay, Valenzuela City', 'Philippines', 'Haircut', '2019-09-10', '16:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `slider`
+--
+
+CREATE TABLE `slider` (
+  `id` int(11) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `caption` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `create_account`
+--
+ALTER TABLE `create_account`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `details_slider`
+--
+ALTER TABLE `details_slider`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `rooms`
+--
+ALTER TABLE `rooms`
+  ADD PRIMARY KEY (`room_id`);
+
+--
+-- Indexes for table `service_reservation_details`
+--
+ALTER TABLE `service_reservation_details`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `slider`
+--
+ALTER TABLE `slider`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `create_account`
+--
+ALTER TABLE `create_account`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `details_slider`
+--
+ALTER TABLE `details_slider`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `rooms`
+--
+ALTER TABLE `rooms`
+  MODIFY `room_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1001;
+
+--
+-- AUTO_INCREMENT for table `service_reservation_details`
+--
+ALTER TABLE `service_reservation_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- AUTO_INCREMENT for table `slider`
+--
+ALTER TABLE `slider`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
